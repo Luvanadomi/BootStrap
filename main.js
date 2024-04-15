89dc2651440a0184844a62be93c01f6fe8b3e662
Luvana Domi <luvanadomi@gmail.com>
2024-04-15 17:13:46 +0200

    created a cart array to add products and organized the files by creating modules,importing and exporting them

--- main.js
+++ main.js
@@ -1,41 +1,9 @@
+import {cart} from '../cart.js';
+import {bestsellers} from '../product.js';
+getBestsellerData();
+ 
+
 function getBestsellerData(){ 
-const bestsellers=[
-    {
-        id:1,
-        name:"VITAL C hydrating anti-aging serum",
-        price:82,
-        image:"../images/ph1.avif",
-    },
-    {
-        id:2,
-        name:"ORMEDIC® balancing facial cleanser",
-        price:102,
-        image:"../images/ph2.avif",
-    },{
-        id:3,
-        name:"AGELESS total pure hyaluronic 6 filler",
-        price:67,
-        image:"../images/ph3.webp",
-    },
-    {
-        id:4,
-        name:"ORMEDIC sheer pink lip enhancement complex",
-        price:98,
-        image:"../images/ph4.webp",
-    },{
-        id:5,
-        name:"VITAL C hydrating  and intensive repair crème",
-        price:56,
-        image:"../images/ph5.webp",
-    },{
-        id:6,
-        name:"VITAL C hydrating intense moisturizer",
-        price:101,
-        image:"../images/ph6.webp",
-    },
-];
-
-
 
 let bestsellerDisplay =document.querySelector('.bestseller-container');
 let bestsellerHtml= '';
@@ -51,7 +19,7 @@ bestsellers.forEach((bestseller) =>{
     <i class="fa-solid fa-star"></i>
     <i class="fa-solid fa-star"></i>
     </span>
-    <button type="button" class="btn btn-sm shopping"><a href="../shopping/shop.html">Buy </a></button>
+    <button class="add-to-cart-btn" data-bestseller-id="${bestseller.id}">Buy</button>
 </div>
 `;
 });
@@ -59,59 +27,6 @@ bestsellerDisplay.innerHTML=bestsellerHtml;
 }
 
 function getFaceData(){ 
-    const faceProducts=[
-        {
-            id:7,
-            name:"SWISS PEPTIDE COMPLEX",
-            price:85,
-            image:"../images/face1.webp",
-        },
-        {
-            id:8,
-            name:"SWISS SUNCARE SPF50+",
-            price:95,
-            image:"../images/face2.webp",
-        },{
-            id:9,
-            name:"SWISS SNOW ALGAE 24H FACE CREAM",
-            price:95,
-            image:"../images/face3.webp",
-        },{
-            id:10,
-            name:"PEPTIDE MULTIPLEX CONCENTRATE",
-            price:110,
-            image:"../images/face4.webp",
-        },
-        {
-            id:11,
-            name:"STEM CELL PLASMA CONCENTRATE",
-            price:95,
-            image:"../images/face5.webp",
-        },{
-            id:12,
-            name:"STEM CELL THERAPY CONCENTRATE",
-            price:85,
-            image:"../images/face6.webp",
-        },{
-            id:13,
-            name:"VENOM THERAPY CONCENTRATE",
-            price:95,
-            image:"../images/face7.webp",
-        },
-        {
-            id:14,
-            name:"TIMELESS FOAMING CLEANSING BALM",
-            price:38,
-            image:"../images/face8.webp",
-        },{
-            id:15,
-            name:"SWISS ROYAL EPIGEN TIMELESS ESSENCE",
-            price:85,
-            image:"../images/face9.webp",
-        }
-    ];
-    
-    
     let faceDisplay =document.querySelector('.face-container');
     let faceHtml= '';
     faceProducts.forEach((faceProduct) =>{
@@ -126,8 +41,8 @@ function getFaceData(){
         <i class="fa-solid fa-star"></i>
         <i class="fa-solid fa-star"></i>
         </span>
-        <button type="button" class="btn btn-sm shopping"><a href="../shopping/shop.html">Buy</a></button>
-    </div>
+        <button class="add-to-cart-btn" data-face-id="${faceProduct.id}">Buy</button>
+        </div>
     `;
     });
     faceDisplay.innerHTML=faceHtml;
@@ -135,59 +50,6 @@ function getFaceData(){
 
     
 function getBodyData(){ 
-    const bodyProducts=[
-        {
-            id:16,
-            name:"SWISS ALPINE GLOW PEELING SCRUB MASK",
-            price:75,
-            image:"../images/body1.webp",
-        },
-        {
-            id:17,
-            name:"SWISS ULTRA PEELING MASK",
-            price:95,
-            image:"../images/body2.webp",
-        },{
-            id:18,
-            name:"BREAKOUT-PRONE SKIN",
-            price:95,
-            image:"../images/body3.webp",
-        },{
-            id:19,
-            name:"SWISS SNOW ALGAE ESSENCE",
-            price:58,
-            image:"../images/body4.webp",
-        },
-        {
-            id:20,
-            name:"SWISS SNOW SILK SOUFLEE CLEANSER",
-            price:58,
-            image:"../images/body5.webp",
-        },{
-            id:21,
-            name:"SWISS ULTRA PEELING MASK",
-            price:110,
-            image:"../images/body6.webp",
-        },{
-            id:22,
-            name:"SWISS OXYGEN THERAPY",
-            price:85,
-            image:"../images/body7.webp",
-        },
-        {
-            id:23,
-            name:"SWISS PEPTIDE MATRIX",
-            price:95,
-            image:"../images/body8.webp",
-        },{
-            id:24,
-            name:"NORMAL SKIN SET",
-            price:85,
-            image:"../images/body9.webp",
-        }
-    ];
-    
-    
     let bodyDisplay =document.querySelector('.body-container');
     let bodyHtml= '';
     bodyProducts.forEach((bodyProduct) =>{
@@ -202,9 +64,46 @@ function getBodyData(){
         <i class="fa-solid fa-star"></i>
         <i class="fa-solid fa-star"></i>
         </span>
-        <button type="button" class="btn btn-sm shopping"><a href="../shopping/shop.html">Buy</a></button>
-    </div>
+        <button class="add-to-cart-btn" data-body-id="${bodyProduct.id}
+        ">Buy</button>
+        </div>
     `;
     });
     bodyDisplay.innerHTML=bodyHtml;
-    }
+} 
+ 
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('.bestseller-container').addEventListener('click', function (event) {
+        if (event.target && event.target.classList.contains('add-to-cart-btn')) {
+            const productId = event.target.getAttribute('data-bestseller-id');
+
+            let matched;
+
+            cart.forEach((item)=>{
+                if(productId===item.productId){
+                    matched=item;
+                }
+            });
+
+            if(matched){
+                matched.quantity+=1;
+            }else{
+
+                cart.push(
+                    {
+                        productId:  productId,
+                        quantity: 1,
+                    });
+            }
+
+            console.log(cart);
+            let quantityCart=0;
+            cart.forEach((item)=>{
+                quantityCart+=item.quantity;
+                
+            });
+            document.querySelector('.js-cart-quantity').innerHTML=quantityCart;
+            console.log(quantityCart);
+     }
+    });
+});
